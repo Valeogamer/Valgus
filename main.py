74f01ca86dd6cd71d83080ff1d4b142925e2095f
Valeogamer <zhilin.valentin.72@gamil.com>
2023-09-20 21:37:48 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+import remove_bg
+if __name__ == '__main__':
+    remove_bg.remove_my_bg()
---
+++ remove_bg.py
@@ -0,0 +1,17 @@
+"""
+На данный момент обработка одного фото занимает 1.5 сек
+"""
+from rembg import remove
+from PIL import Image
+import time
+
+def remove_my_bg():
+    start = time.time()
+    input_path = 'images/ (168).jpg'
+    output_path = 'iamge_foot_1.png'
+
+    input = Image.open(input_path)
+    output = remove(input)
+    output.save(output_path)
+    end = time.time()
+    print(end - start)
