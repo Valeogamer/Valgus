906334c796ed0b38da1eb84deea26a23f9393534
Valeogamer <zhilin.valentin.72@gamil.com>
2023-09-28 23:58:08 +0300

    feat: наброски, попытки реализии бинаризаци ч/б img

--- remove_bg.py
+++ remove_bg.py
@@ -1,17 +1,23 @@
 """
+Модуль обработки изображения
+Удаление заднего фона
 На данный момент обработка одного фото занимает 1.5 сек
 """
 from rembg import remove
 from PIL import Image
-import time
+from time import perf_counter
 
 def remove_my_bg():
-    start = time.time()
+    """
+    Удаление заднего фона
+    :return:
+    """
+    start = perf_counter()
     input_path = 'images/ (168).jpg'
-    output_path = 'iamge_foot_1.png'
+    output_path = 'image_foot_3.png'
 
     input = Image.open(input_path)
     output = remove(input)
     output.save(output_path)
-    end = time.time()
+    end = perf_counter()
     print(end - start)
