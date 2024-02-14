3b74bfeaa54ff36b1cf4bd86c96506779a63e074
Valentin <zhilin.valentin.72@gmail.com>
2024-02-14 19:44:12 +0300

    *FastResize

---
+++ FastResize.py
@@ -0,0 +1,22 @@
+from PIL import Image
+import os
+
+# Путь к каталогу с изображениями
+directory = "C://Diplom//AllFootSize640//"
+
+# Получаем список всех файлов в каталоге
+files = os.listdir(directory)
+
+# Проходим через каждый файл
+for file in files:
+    # Проверяем, что файл - изображение
+    if file.endswith(('jpeg', 'jpg', 'png', 'gif')):
+        # Открываем изображение
+        image_path = os.path.join(directory, file)
+        img = Image.open(image_path)
+
+        # Изменяем размер до 640x640
+        img_resized = img.resize((640, 640))
+
+        # Сохраняем измененное изображение с тем же именем
+        img_resized.save(image_path)
--- OpenCVMethods/AngelPronation.py
+++ OpenCVMethods/AngelPronation.py
@@ -1,6 +1,5 @@
 import cv2
 import matplotlib.pyplot as plt
-import typing as tp
 
 
 class Foot:
